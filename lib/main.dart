cff4d72dc75d22e883509ce30e7a6c45606f3447
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-02-06 22:33:04 +0530

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:laborlane/OnBoarding_Pages/first_screan.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-     
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FirstScrean(),
     );
   }
 }
