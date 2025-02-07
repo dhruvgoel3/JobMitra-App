81b2bbfe07ea3cdf07e3606c759b897ab833be75
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-02-07 19:48:09 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:laborlane/OnBoarding_Pages/first_screan.dart';
 
 void main() {
@@ -11,9 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'LaborLane',
       home: FirstScrean(),
     );
   }
