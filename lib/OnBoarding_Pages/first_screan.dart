2b2a70749eba8bb005a6812104f05e7b2369d864
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-02-07 20:29:13 +0530

    changes

--- lib/OnBoarding_Pages/first_screan.dart
+++ lib/OnBoarding_Pages/first_screan.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:laborlane/Widgets/round_button.dart';
+
 class FirstScrean extends StatelessWidget {
   const FirstScrean({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(child: RoundButton(title: "GET STARTED", onTap: () {}))
+        ],
+      ),
+    );
   }
 }
