4ff7b865e67b7f7a6733bcfaab5c1bc7805e3790
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-02-08 16:09:51 +0530

    changes

--- lib/OnBoarding_Pages/first_screan.dart
+++ lib/OnBoarding_Pages/first_screan.dart
@@ -3,7 +3,6 @@ import 'package:laborlane/Widgets/round_button.dart';
 import 'package:laborlane/Widgets/textfields.dart';
 
 class FirstScrean extends StatelessWidget {
- 
   const FirstScrean({super.key});
 
   @override
@@ -21,18 +20,44 @@ class FirstScrean extends StatelessWidget {
                     begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
                     colors: [Color(0xfff7cda5), Color(0xfffaecd4)])),
-
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(right: 319,top: 140),
+                  padding: const EdgeInsets.only(right: 319, top: 140),
                   child: Image.asset("assets/images/saw.png"),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 65),
+                  child: Container(
+                    height: 380,
+                    width: 161,
+                    decoration: BoxDecoration(),
+                    child: Image.asset("assets/images/worker with boxes.png"),
+                  ),
+                ),
                 Container(
-                  height: 380,
-                  width: 161,
-                  decoration: BoxDecoration(),
-                  child: Image.asset("assets/images/worker with boxes.png"),
+                  height: 113,
+                  width: 160,
+                  decoration: BoxDecoration(color: Colors.white),
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Connecting workers to",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        Text("Jobs Seamlessly",style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black),)
+                      ],
+                    )
+                  ],
                 )
               ],
             ),
