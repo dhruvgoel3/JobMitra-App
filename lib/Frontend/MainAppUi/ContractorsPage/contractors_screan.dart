bab244e4e14670b1cd379382d36346daeb35ca15
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-03-12 22:35:53 +0530

    changes

--- lib/Frontend/MainAppUi/ContractorsPage/contractors_screan.dart
+++ lib/Frontend/MainAppUi/ContractorsPage/contractors_screan.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class ContractorsScrean extends StatefulWidget {
   const ContractorsScrean({super.key});
 
@@ -9,6 +11,30 @@ class ContractorsScrean extends StatefulWidget {
 class _ContractorsScreanState extends State<ContractorsScrean> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10))),
+        backgroundColor: Color(0xfffae9d0),
+        leading: Image.asset("assets/images/ph_list-bold.png", height: 20),
+        title: Text(
+          "Contractors",
+          style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w600, fontSize: 25, color: Colors.black),
+        ),
+        titleSpacing: 3,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Image.asset(
+              "assets/images/mingcute_notification-fill.png",
+              height: 27,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
