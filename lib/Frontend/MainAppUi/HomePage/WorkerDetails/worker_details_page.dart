f6abe12179bbc9459a3b372ef6d163bc00c0dafa
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-03-13 22:53:11 +0530

    changes

---
+++ lib/Frontend/MainAppUi/HomePage/WorkerDetails/worker_details_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class WorkerDetailsPage extends StatefulWidget {
+  const WorkerDetailsPage({super.key});
+
+  @override
+  State<WorkerDetailsPage> createState() => _WorkerDetailsPageState();
+}
+
+class _WorkerDetailsPageState extends State<WorkerDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
