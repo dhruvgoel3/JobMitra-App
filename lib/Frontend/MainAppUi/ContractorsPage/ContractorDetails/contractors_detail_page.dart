bc0cb3ca4c504af25b3e7b316818eb18d985410d
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-03-14 22:58:29 +0530

    changes

---
+++ lib/Frontend/MainAppUi/ContractorsPage/ContractorDetails/contractors_detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ContractorsDetailPage extends StatefulWidget {
+  const ContractorsDetailPage({super.key});
+
+  @override
+  State<ContractorsDetailPage> createState() => _ContractorsDetailPageState();
+}
+
+class _ContractorsDetailPageState extends State<ContractorsDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
