f4034a66f8b0e666d32072f319981f0cd8d7c69c
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-03-16 17:47:16 +0530

    changes

---
+++ lib/Frontend/MainAppUi/Hiring_Interface/MessagesPage/chats_listview.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ChatsListview extends StatefulWidget {
+  const ChatsListview({super.key});
+
+  @override
+  State<ChatsListview> createState() => _ChatsListviewState();
+}
+
+class _ChatsListviewState extends State<ChatsListview> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Typing....",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.amber),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+        ListTile(
+          onTap: () {},
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(
+                "assets/images/indian-construction-worker-painting-a-wall-in-an-industrial-plant-BE355K 1.png"),
+            backgroundColor: Colors.green,
+            radius: 25,
+          ),
+          title: Text(
+            "Ramesh",
+            style: GoogleFonts.poppins(
+                fontSize: 17, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          subtitle: Text(
+            "Hurry up Ramesh !",
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+          ),
+          trailing: Text(
+            "1m",
+            style: GoogleFonts.poppins(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+      ],
+    );
+  }
+}
