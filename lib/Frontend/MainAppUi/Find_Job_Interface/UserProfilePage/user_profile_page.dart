e81f5c40884e9bf69cc21ad146d12d220bb52ea5
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-03-18 17:57:32 +0530

    changes

--- lib/Frontend/MainAppUi/Find_Job_Interface/UserProfilePage/user_profile_page.dart
+++ lib/Frontend/MainAppUi/Find_Job_Interface/UserProfilePage/user_profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:laborlane/Frontend/MainAppUi/Find_Job_Interface/UserProfilePage/user_intro_part.dart';
 
 class UserProfilePage extends StatefulWidget {
   const UserProfilePage({super.key});
@@ -12,6 +13,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
@@ -28,118 +30,160 @@ class _UserProfilePageState extends State<UserProfilePage> {
         actions: [
           Padding(
               padding: const EdgeInsets.only(right: 20),
-              child: Icon(Icons.gpp_good))
+              child: Icon(
+                Icons.share,
+                color: Colors.black,
+              ))
         ],
       ),
       body: Column(
         children: [
+          UserIntroPart(),
+          SizedBox(
+            height: 5,
+          ),
+          ListTile(
+            tileColor: Colors.grey.withOpacity(0.1),
+            title: Text(
+              "Daily rate",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey,
+                  fontSize: 15),
+            ),
+            subtitle: Text(
+              "\$50/day",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                  fontSize: 19),
+            ),
+            trailing: Container(
+              width: 130,
+              height: 30,
+              child: Center(
+                child: Text(
+                  "Explore Jobs",
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontSize: 16),
+                ),
+              ),
+              decoration: BoxDecoration(
+                  color: Color(0xFFF2A160),
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
           Container(
-            height: 165,
-            child: Card(
-              shadowColor: Colors.black,
-              borderOnForeground: true,
-              color: Colors.white,
-              child: Column(
+            color: Colors.grey.withOpacity(0.1),
+            child: Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Row(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, top: 5),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.blueGrey,
-                        ),
-                        Text(
-                          "Mullana,Ambala",
-                          style: GoogleFonts.poppins(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xff6b747c)),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
+                  Column(
                     children: [
-                      SizedBox(
-                        width: 15,
+                      Text(
+                        "4.8",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                            fontSize: 17),
                       ),
-                      CircleAvatar(
-                        backgroundImage: AssetImage(
-                          "assets/images/images 1.png",
-                        ),
-                        radius: 50,
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 15.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Rahul Sharma",
-                              style: GoogleFonts.inter(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 21,
-                                  color: Colors.black),
-                            ),
-                            Text(
-                              "Electrician",
-                              style: GoogleFonts.inter(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 15,
-                                  color: Colors.grey),
-                            ),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                  size: 20,
-                                ),
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                  size: 20,
-                                ),
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                  size: 20,
-                                ),
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                  size: 20,
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  "4.1 (440 reviews)",
-                                  style: GoogleFonts.inter(
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 15,
-                                      color: Colors.grey),
-                                ),
-                              ],
-                            ),
-                            Text(
-                              "+91-9857385898",
-                              style: GoogleFonts.inter(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 15,
-                                  color: Colors.grey),
-                            ),
-                          ],
-                        ),
+                      Text(
+                        "Rating",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey,
+                            fontSize: 14),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: 45,
+                  ),
+                  Container(
+                    height: 60,
+                    width: 0.5,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(
+                    width: 45,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "120",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                            fontSize: 17),
                       ),
+                      Text(
+                        "Jobs Done",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey,
+                            fontSize: 14),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    width: 45,
+                  ),
+                  Container(
+                    height: 60,
+                    color: Colors.grey,
+                    width: 0.5,
+                  ),
+                  SizedBox(
+                    width: 45,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "5+",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                            fontSize: 17),
+                      ),
+                      Text(
+                        "Years",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey,
+                            fontSize: 14),
+                      )
                     ],
                   ),
                 ],
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(right: 225,top: 13),
+            child: Text(
+              "Work Portfolio",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  fontSize: 17),
+            ),
+          ),
+          Row(
+            children: [
+              Image.asset("")
+
+            ],
+          )
         ],
       ),
     );
