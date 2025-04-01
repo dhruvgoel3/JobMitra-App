9c5d28c80d5505d4db17ccbf7d0895ca37fdf7cc
RepoByDhruv <dhruvgoel1100@gmail.com>
2025-04-01 20:36:39 +0530

    changes

--- lib/Utils/colors.dart
+++ lib/Utils/colors.dart
@@ -2,3 +2,6 @@ import 'dart:ui';
 
 // final  Color = Color(0xfffae9d0);  // light cream
 // final  Color = Color(0xFFF2A160);  // dark cream
+
+
+// Here are the colors which are used repeatly in our app
